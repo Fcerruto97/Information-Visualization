862cd3a8da04ba928a9b56e1bf4c9cc324cf4a9b
Simone <sim.gam997@gmail.com>
2021-01-25 19:36:30 +0100

    add IEnumerator func to ghost

--- pacman/Assets/EnemyAi.cs
+++ pacman/Assets/EnemyAi.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -17,7 +17,7 @@ public class EnemyAi: MonoBehaviour
     public Vector3 walkPoint;
     bool walkPointSet = true;
     public float walkPointRange;
-
+    public bool awake = false;
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();        
@@ -25,7 +25,6 @@ public class EnemyAi: MonoBehaviour
 
     private void Awake()
     {
-        StartCoroutine(TimerAwake());
         player = GameObject.Find("Chomp").transform;
         agent = this.GetComponent<NavMeshAgent>();
         agent.SetDestination(position.position);
@@ -34,10 +33,20 @@ public class EnemyAi: MonoBehaviour
 
     private void Update()
     {
+        if(awake) StartCoroutine(TimerAwake());
         Vector3 distanceToPacman = this.transform.position - player.transform.position;
 
-        if (distanceToPacman.magnitude > 6f) { Patroling(); Debug.Log("patrol "+ agent.name); }
-            else { ChasePlayer(); walkPointSet = false; Debug.Log("Chase" + agent.name); }
+        if (distanceToPacman.magnitude > 6f)
+        {
+            Patroling();
+            //Debug.Log("Patrol "+ agent.name);
+        }
+        else
+        {
+            ChasePlayer();
+            walkPointSet = false;
+            //Debug.Log("Chase " + agent.name); 
+        }
     }
 
     private void Patroling()
@@ -48,7 +57,7 @@ public class EnemyAi: MonoBehaviour
         Vector3 distance3 = this.transform.position - agent3.transform.position;
         Vector3 distance4 = this.transform.position - agent4.transform.position;
 
-        if (distance2.magnitude < 1f || distance3.magnitude < 1f || distance4.magnitude < 1f)
+        if (distance2.magnitude < 1.5f || distance3.magnitude < 1.5f || distance4.magnitude < 1.5f)
         {
             SearchWalkPoint();
         }
@@ -71,7 +80,7 @@ public class EnemyAi: MonoBehaviour
         if ((transform.position.z + randomZ) > 10f || (transform.position.z + randomZ) < -10f) { randomZ = 0.5f; }
 
         if (Random.Range(0, 4) % 2 == 0) { randomChange = randomChange * -1; };
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, (transform.position.z + randomZ)* randomChange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, (transform.position.z + randomZ) * randomChange);
         walkPointSet = true;
 
         if (Physics.Raycast(walkPoint, -transform.up, 8f, whatIsGround))
@@ -90,5 +99,15 @@ public class EnemyAi: MonoBehaviour
 
     private IEnumerator TimerAwake()
     {
+        awake = false;
+        float countdown = Random.Range(0f, 5f);
+
+        while (countdown > 0)
+        {
+            if(agent.name.Contains("Orange"))
+                Debug.Log("countdown "+agent.name +": "+ countdown);
+            countdown -= Time.deltaTime;
+        }
+        yield return null;
     }
 }
